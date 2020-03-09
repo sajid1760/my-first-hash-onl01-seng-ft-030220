8cd5b0f0f8a736e7203fda918b5e3e9119b56865
Sajid Anjum <sajid1760@gmail.com>
2020-03-09 20:48:12 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -5,9 +5,8 @@ end
 
 
 def shipping_manifest
-  the_manifest = {"whale bone corsets" => 5, 
-
-
+  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2,
+  "oil paintings" => 3} 
 end
 
 def retrieval
@@ -16,9 +15,7 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -27,14 +24,7 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
+  shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 4
+  shipping_manifest
 end
