11c8c4c6789957dfaab28d079ebcfb50d1cc98fd
Sajid Anjum <sajid1760@gmail.com>
2020-03-09 20:44:48 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,14 +1,11 @@
 def my_hash
-  your_hash = {myname => "Sajid"
-
-  
+  your_hash = {"myname" => "Sajid", "myage" => "38", "myapartment" => "Tarrance"}
+  your_hash
 end
 
 
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
+  the_manifest = {"whale bone corsets" => 5, 
 
 
 end
